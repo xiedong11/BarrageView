0298853be97ab2c1752719c1cfb0fe3449def085
xiedong <zx381010>
2018-09-07 15:43:53 +0800

    添加example样例

---
+++ barrageexample/src/main/java/com/zhuandian/barrageexample/BarrageItem.java
@@ -0,0 +1,13 @@
+package com.zhuandian.barrageexample;
+
+import android.widget.TextView;
+
+public class BarrageItem {
+    public TextView textView;
+    public int textColor;
+    public String text;
+    public int textSize;
+    public int moveSpeed;//移动速度
+    public int verticalPos;//垂直方向显示的位置
+    public int textMeasuredWidth;//字体显示占据的宽度
+}
---
+++ barrageexample/src/main/java/com/zhuandian/barrageexample/IBarrageItemClickListener.java
@@ -0,0 +1,10 @@
+package com.zhuandian.barrageexample;
+
+/**
+ * desc :
+ * author：xiedong
+ * data：2018/9/7
+ */
+public interface IBarrageItemClickListener {
+    void onItemClick(String content);
+}
